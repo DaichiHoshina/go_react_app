eb2cf7290b0e0d8a90ba8d4363334cceb3436af5
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-05-14 21:15:02 +0900

    frontにコピー

--- adminのコピー/Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM node:14.15.3-alpine
-WORKDIR /usr/src/app
--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -1,41 +1,87 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { useEffect, useRef, useState } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Box from "@material-ui/core/Box";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+import { useRouter } from "next/router";
+import SideMenuArea from "./templates/SideMenuArea";
+import Footer from "./organisms/common/Footer";
 
-type Props = {
-  children?: ReactNode
-  title?: string
+const drawerWidth = 240;
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © Daichi Hoshina"}
+    </Typography>
+  );
+};
+
+export interface LayoutProps {
+  children: React.ReactNode;
+  title: string;
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+const Layout: React.FC<LayoutProps> = ({ children, title }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const sideMenuRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  const [sideMenuWidth, setSideMenuWidth] = useState(0);
 
-export default Layout
+  const handleDrawerOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    if (sideMenuRef.current !== null) {
+      setSideMenuWidth(sideMenuRef.current.clientWidth);
+    }
+  });
+
+  return (
+    <div className="min-h-screen bg-blue-50 pt-10">
+      <CssBaseline />
+      <AppBar style={{ zIndex: 1201 }} position="fixed">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className="flex-grow">
+            管理画面
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div
+        style={{
+          paddingLeft:
+            isOpen && !router.pathname.includes("/login") ? sideMenuWidth : 0,
+        }}
+      >
+        <div className="p-7">
+          <Typography component="h2" variant="h6" color="inherit">
+            {title}
+          </Typography>
+          {children}
+        </div>
+      </div>
+      <Box className="pt-8">
+        <Footer />
+      </Box>
+      <Drawer open={isOpen} variant="persistent" anchor="left">
+        <div ref={sideMenuRef} />
+        <SideMenuArea />
+      </Drawer>
+    </div>
+  );
+};
+
+export default Layout;
