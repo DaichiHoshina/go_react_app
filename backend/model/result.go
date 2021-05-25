652cfbf72d2daf1725188589208f2a03839a8cf1
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-05-25 15:39:04 +0900

    修正

--- backend/model/result.go
+++ backend/model/result.go
@@ -1,16 +1,6 @@
 package model
 
-import (
-	"gorm.io/gorm"
-)
-
 type Results struct {
-	Result Result `json:"presentations"`
-}
-
-type Result struct {
-	gorm.Model
 	Users
 	Presentations
-	OtherName string
 }
