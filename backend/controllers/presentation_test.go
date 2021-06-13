4cb266734a4d8f445c7d1e095f84a4bf1b431874
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-06-13 15:57:21 +0900

    presentationのテスト作成

--- backend/controllers/presentation_test.go
+++ backend/controllers/presentation_test.go
@@ -1,51 +1,143 @@
-import "testing"
+package controllers
 
-type Presentation struct{}
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"testing"
+)
 
-func (u *Presentation) FindAll() ([]model.Presentation, error) {
-	presentations := []model.Presentation{}
-	t, _ := time.Parse("2006-01-02", "2021-01-01")
-
-	presentation1 := model.Presentation{Title: "Go言語の本", Discription: "誰か"}
-	presentation1.ID = 1
-	presentation1.UserId = 1
-	presentation1.CreatedAt = t
-	presentation1.UpdatedAt = t
-	presentations = append(presentations, presentation1)
-
-	// b2 := model.Presentation{Title: "Go言語の本2", Author: "誰か2"}
-	// b2.ID = 2
-	// b2.CreatedAt = t
-	// b2.UpdatedAt = t
-	// presentations = append(presentations, b2)
-
-	return presentations, nil
+func TestGetPresentations(t *testing.T) {
+	url := "http://localhost:3001" + "/presentations"
+	res, err := http.Get(url)
+	if err != nil {
+		t.Errorf("Expected nil, got %v", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code is %d, got %d", http.StatusOK, res.StatusCode)
+	}
 }
 
-func TestGetIndex(t *testing.T) {
-	e := echo.New()
-	presentations := &Presentation{}
-	h := NewPresentationHandler(presentations)
-	e.GET("/presentations", h.GetIndex)
+func TestGetPresentation(t *testing.T) {
+	url := "http://localhost:3001" + "/presentations/1"
+	res, err := http.Get(url)
+	if err != nil {
+		t.Errorf("Expected nil, got %v", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code is %d, got %d", http.StatusOK, res.StatusCode)
+	}
+}
 
-	apitest.New().
-		Handler(e).
-		Get("/presentations").
-		Expect(t).
-		Body(`
-          {
-              "Presentations": [
-                  {
-                      "ID": 1,
-                      "CreatedAt": "2021-01-01T00:00:00Z",
-                      "UpdatedAt": "2021-01-01T00:00:00Z",
-                      "DeletedAt": null,
-                      "Title": "Go言語の本",
-                      "Discription": "誰か"
-                  },
-              ]
-          }
-      `).
-		Status(http.StatusOK).
-		End()
+func TestCreatePresentation(t *testing.T) {
+
+	url := "http://localhost:3001" + "/presentations"
+
+	fieldname := "file"
+	filename := "test.jpg"
+	file, err := os.Open(filename)
+
+	body := &bytes.Buffer{}
+	// データのmultipartエンコーディングを管理するmultipart.Writerを生成する。
+	// ランダムなbase-16バウンダリが生成される。
+	mw := multipart.NewWriter(body)
+
+	err = mw.WriteField("user_id", "1")
+
+	fw, err := mw.CreateFormFile(fieldname, filename)
+
+	// fwで作ったパートにファイルのデータを書き込む
+	_, err = io.Copy(fw, file)
+
+	// リクエストのContent-Typeヘッダに使う値を取得する（バウンダリを含む）
+	contentType := mw.FormDataContentType()
+
+	// 書き込みが終わったので最終のバウンダリを入れる
+	err = mw.Close()
+
+	res, err := http.Post(url, contentType, body)
+	res.Header.Add("Content-Type", "multipart/form-data")
+	if err != nil {
+		t.Errorf("Expected nil, got %v", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code is %d, got %d", http.StatusOK, res.StatusCode)
+	}
+}
+
+// func TestUpdatePresentation(t *testing.T) {
+
+// 	url := "http://localhost:3001" + "/presentations"
+
+// 	fieldname := "file"
+// 	filename := "test.jpg"
+// 	file, err := os.Open(filename)
+
+// 	body := &bytes.Buffer{}
+// 	// データのmultipartエンコーディングを管理するmultipart.Writerを生成する。
+// 	// ランダムなbase-16バウンダリが生成される。
+// 	mw := multipart.NewWriter(body)
+
+// 	err = mw.WriteField("user_id", "1")
+
+// 	fw, err := mw.CreateFormFile(fieldname, filename)
+
+// 	// fwで作ったパートにファイルのデータを書き込む
+// 	_, err = io.Copy(fw, file)
+
+// 	// リクエストのContent-Typeヘッダに使う値を取得する（バウンダリを含む）
+// 	contentType := mw.FormDataContentType()
+
+// 	// 書き込みが終わったので最終のバウンダリを入れる
+// 	err = mw.Close()
+
+// 	res, err := http.Put(url, contentType, body)
+// 	res.Header.Add("Content-Type", "multipart/form-data")
+// 	if err != nil {
+// 		t.Errorf("Expected nil, got %v", err)
+// 	}
+// 	if res.StatusCode != http.StatusOK {
+// 		t.Errorf("Expected status code is %d, got %d", http.StatusOK, res.StatusCode)
+// 	}
+// }
+
+func TestDeletePresentation(t *testing.T) {
+
+	url := "http://localhost:3001" + "/presentaons/1"
+
+	// Create client
+	client := &http.Client{}
+
+	// res, err := http.Delete(url, contentType, nil)
+	// res.Header.Add("Content-Type", "multipart/form-data")
+	// if err != nil {
+	// 	t.Errorf("Expected nil, got %v", err)
+	// }
+	// if res.StatusCode != http.StatusOK {
+	// 	t.Errorf("Expected status code is %d, got %d", http.StatusOK, res.StatusCode)
+	// }
+	// Create request
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Fetch Request
+	resp, err := client.Do(req)
+	if resp.StatusCode != 400 {
+		fmt.Println(err)
+		fmt.Println(resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	// // Read Response Body
+	// respBody, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 }
