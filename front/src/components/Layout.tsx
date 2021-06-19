344ed29304b7b88fc9ac9ddcd12ae0836426374f
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-06-19 14:01:24 +0900

    エラー解消

--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -11,7 +11,6 @@ import HeaderUserIcon from "../components/atoms/share/HeaderUserIcon";
 import { loginConfirm } from "../services/User";
 import { Provider, useDispatch } from "react-redux";
 import LoginIcon from "./atoms/share/LoginIcon";
-import store from "../store";
 export interface LayoutProps {
   children: React.ReactNode;
   title: string;
@@ -33,43 +32,37 @@ export const Layout: React.FC<LayoutProps> = ({ children, title }) => {
   }, []);
 
   return (
-    <Provider store={store()}>
-      <div className="min-h-screen pt-10">
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          style={{ zIndex: 1201, color: "#FFFFFF", backgroundColor: "#000000" }}
-        >
-          <div className="flex">
-            <div className="mt-2">
-              <Toolbar variant="dense">
-                <Link href="/presentations" color="inherit" underline="none">
-                  <Typography
-                    variant="h6"
-                    color="inherit"
-                    className="flex-grow"
-                  >
-                    Repgram
-                  </Typography>
-                </Link>
-              </Toolbar>
-            </div>
-            <div className="ml-auto mt-2 mr-2">
-              {isLogin ? <HeaderUserIcon /> : <LoginIcon />}
-            </div>
+    <div className="min-h-screen pt-10">
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        style={{ zIndex: 1201, color: "#FFFFFF", backgroundColor: "#000000" }}
+      >
+        <div className="flex">
+          <div className="mt-2">
+            <Toolbar variant="dense">
+              <Link href="/presentations" color="inherit" underline="none">
+                <Typography variant="h6" color="inherit" className="flex-grow">
+                  Repgram
+                </Typography>
+              </Link>
+            </Toolbar>
           </div>
-        </AppBar>
-        <div className="p-7 mt-3">
-          <Typography component="h2" variant="h6" color="inherit">
-            {title}
-          </Typography>
-          {children}
+          <div className="ml-auto mt-2 mr-2">
+            {isLogin ? <HeaderUserIcon /> : <LoginIcon />}
+          </div>
         </div>
-        <Box className="pt-8">
-          <Footer />
-        </Box>
+      </AppBar>
+      <div className="p-7 mt-3">
+        <Typography component="h2" variant="h6" color="inherit">
+          {title}
+        </Typography>
+        {children}
       </div>
-    </Provider>
+      <Box className="pt-8">
+        <Footer />
+      </Box>
+    </div>
   );
 };
 
