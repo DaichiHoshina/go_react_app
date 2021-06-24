0cb2d80dda109ba1db9b1545b83ff5f98695badf
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-06-24 13:33:35 +0900

    不要箇所削除

--- front/src/components/__tests__/SignUpForm_test.tsx
+++ front/src/components/__tests__/SignUpForm_test.tsx
@@ -1,5 +1,4 @@
 import "@testing-library/jest-dom/extend-expect";
-import React from "react";
 import { Provider } from "react-redux";
 import renderer from "react-test-renderer";
 import store from "../../store";
